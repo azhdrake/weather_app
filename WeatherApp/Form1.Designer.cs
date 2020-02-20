581bd42a88d92b5502ee4bb7a17773fb6ba8b1dd
B33 <my@email.com>
2020-02-20 13:16:58 -0600

    Add project files.

---
+++ WeatherApp/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WeatherApp
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.pbWeatherImage = new System.Windows.Forms.PictureBox();
+      this.lblWeatherText = new System.Windows.Forms.Label();
+      this.txtCity = new System.Windows.Forms.TextBox();
+      this.txtState = new System.Windows.Forms.TextBox();
+      this.btbGetWeather = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      ((System.ComponentModel.ISupportInitialize)(this.pbWeatherImage)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // pbWeatherImage
+      // 
+      this.pbWeatherImage.Location = new System.Drawing.Point(-1, 124);
+      this.pbWeatherImage.Name = "pbWeatherImage";
+      this.pbWeatherImage.Size = new System.Drawing.Size(803, 456);
+      this.pbWeatherImage.TabIndex = 0;
+      this.pbWeatherImage.TabStop = false;
+      // 
+      // lblWeatherText
+      // 
+      this.lblWeatherText.Location = new System.Drawing.Point(196, 170);
+      this.lblWeatherText.Name = "lblWeatherText";
+      this.lblWeatherText.Size = new System.Drawing.Size(425, 205);
+      this.lblWeatherText.TabIndex = 99;
+      // 
+      // txtCity
+      // 
+      this.txtCity.Location = new System.Drawing.Point(26, 65);
+      this.txtCity.MaxLength = 50;
+      this.txtCity.Name = "txtCity";
+      this.txtCity.Size = new System.Drawing.Size(189, 31);
+      this.txtCity.TabIndex = 0;
+      // 
+      // txtState
+      // 
+      this.txtState.Location = new System.Drawing.Point(283, 65);
+      this.txtState.MaxLength = 13;
+      this.txtState.Name = "txtState";
+      this.txtState.Size = new System.Drawing.Size(189, 31);
+      this.txtState.TabIndex = 1;
+      // 
+      // btbGetWeather
+      // 
+      this.btbGetWeather.Location = new System.Drawing.Point(558, 58);
+      this.btbGetWeather.Name = "btbGetWeather";
+      this.btbGetWeather.Size = new System.Drawing.Size(144, 44);
+      this.btbGetWeather.TabIndex = 2;
+      this.btbGetWeather.Text = "Get Weather";
+      this.btbGetWeather.UseVisualStyleBackColor = true;
+      this.btbGetWeather.Click += new System.EventHandler(this.btbGetWeather_Click);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(21, 27);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(49, 25);
+      this.label1.TabIndex = 5;
+      this.label1.Text = "City";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(278, 27);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(62, 25);
+      this.label2.TabIndex = 6;
+      this.label2.Text = "State";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 580);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.btbGetWeather);
+      this.Controls.Add(this.txtState);
+      this.Controls.Add(this.txtCity);
+      this.Controls.Add(this.lblWeatherText);
+      this.Controls.Add(this.pbWeatherImage);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      ((System.ComponentModel.ISupportInitialize)(this.pbWeatherImage)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.PictureBox pbWeatherImage;
+    private System.Windows.Forms.Label lblWeatherText;
+    private System.Windows.Forms.TextBox txtCity;
+    private System.Windows.Forms.TextBox txtState;
+    private System.Windows.Forms.Button btbGetWeather;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+  }
+}
+
---
+++ WeatherApp/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Diagnostics;
+
+namespace WeatherApp
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void btbGetWeather_Click(object sender, EventArgs e)
+    {
+      btbGetWeather.Enabled = false;
+
+      string city = txtCity.Text;
+      string state = txtState.Text;
+
+      if (LocationDataValid(city, state, out string error))
+      {
+        if (VerifyLength(city, 3))
+        {
+          if(VerifyLength(state, 2))
+          { 
+            if (VerifyStateName(state)) 
+            { 
+              if (CheckForNumericCharacters(city) && CheckForNumericCharacters(state))
+              {
+                if (GetWeatherText(city, state, out string weather, out string textErrorMessage))
+                {
+                  lblWeatherText.Text = weather;
+                }
+                else
+                {
+                  MessageBox.Show(textErrorMessage, "Error");
+                }
+              }
+              else
+              {
+                MessageBox.Show("Please don't include numbers city and state inputs", "Error");
+              }
+            }
+            else
+            {
+              MessageBox.Show("State not valid.", "Error");
+            }
+          }
+          else
+          {
+            MessageBox.Show("State name needs to be at least 2 characters long", "Error");
+          }
+        }
+        else
+        {
+          MessageBox.Show("City name needs to be at least 3 characters long.", "Error");
+        }
+      }
+      else
+      {
+        MessageBox.Show(error, "Error");
+      }
+      btbGetWeather.Enabled = true;
+    }
+
+    private bool VerifyLength(string toVerify, int minLength)
+    {
+      if (toVerify.Length >= minLength)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+
+    private bool GetWeatherText(string city, string state, out string weatherText, out string errorMessage)
+    {
+      string baseUrl = "http://weather-csharp.herokuapp.com/";
+      string weatherTextUrl = String.Format("{0}text?city={1}&state={2}", baseUrl, city, state);
+      weatherText = null;
+      errorMessage = null;
+
+      try
+      {
+        using (WebClient client = new WebClient())
+        {
+          weatherText = client.DownloadString(weatherTextUrl);
+          return true;
+        }
+        Debug.WriteLine(weatherText);
+      }
+      catch (Exception e)
+      {
+        Debug.WriteLine(e.StackTrace);
+        errorMessage = e.Message;
+        return false;
+      }
+    }
+    private bool LocationDataValid(string city, string state, out string errorMessage)
+    {
+      errorMessage = null;
+      if (String.IsNullOrWhiteSpace(city))
+      {
+        errorMessage = "Please input a value for 'city'";
+        return false;
+      }
+      if (String.IsNullOrWhiteSpace(state))
+      {
+        errorMessage = "Please input a value for 'state'";
+        return false;
+      }
+      return true;
+    }
+    private bool CheckForNumericCharacters(string testString)
+    {
+      foreach (char c in testString)
+      {
+        if (Char.IsNumber(c))
+        {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    private bool VerifyStateName(string potentialState)
+    {
+      Dictionary<string, string> states = GetAllStates();
+      foreach (KeyValuePair<string, string> state in states)
+      {
+        if(potentialState.ToLower() == state.Key.ToLower() || potentialState.ToLower() == state.Value.ToLower())
+        {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    private Dictionary<string, string> GetAllStates()
+      {
+      Dictionary<string, string> states = new Dictionary<string, string>();
+
+      states.Add("AL", "Alabama");
+      states.Add("AK", "Alaska");
+      states.Add("AZ", "Arizona");
+      states.Add("AR", "Arkansas");
+      states.Add("CA", "California");
+      states.Add("CO", "Colorado");
+      states.Add("CT", "Connecticut");
+      states.Add("DE", "Delaware");
+      states.Add("DC", "District of Columbia");
+      states.Add("FL", "Florida");
+      states.Add("GA", "Georgia");
+      states.Add("HI", "Hawaii");
+      states.Add("ID", "Idaho");
+      states.Add("IL", "Illinois");
+      states.Add("IN", "Indiana");
+      states.Add("IA", "Iowa");
+      states.Add("KS", "Kansas");
+      states.Add("KY", "Kentucky");
+      states.Add("LA", "Louisiana");
+      states.Add("ME", "Maine");
+      states.Add("MD", "Maryland");
+      states.Add("MA", "Massachusetts");
+      states.Add("MI", "Michigan");
+      states.Add("MN", "Minnesota");
+      states.Add("MS", "Mississippi");
+      states.Add("MO", "Missouri");
+      states.Add("MT", "Montana");
+      states.Add("NE", "Nebraska");
+      states.Add("NV", "Nevada");
+      states.Add("NH", "New Hampshire");
+      states.Add("NJ", "New Jersey");
+      states.Add("NM", "New Mexico");
+      states.Add("NY", "New York");
+      states.Add("NC", "North Carolina");
+      states.Add("ND", "North Dakota");
+      states.Add("OH", "Ohio");
+      states.Add("OK", "Oklahoma");
+      states.Add("OR", "Oregon");
+      states.Add("PA", "Pennsylvania");
+      states.Add("RI", "Rhode Island");
+      states.Add("SC", "South Carolina");
+      states.Add("SD", "South Dakota");
+      states.Add("TN", "Tennessee");
+      states.Add("TX", "Texas");
+      states.Add("UT", "Utah");
+      states.Add("VT", "Vermont");
+      states.Add("VA", "Virginia");
+      states.Add("WA", "Washington");
+      states.Add("WV", "West Virginia");
+      states.Add("WI", "Wisconsin");
+      states.Add("WY", "Wyoming");
+
+      return states;
+    }
+  }
+}
